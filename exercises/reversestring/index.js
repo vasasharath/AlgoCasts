fa4c69bb5c9f56fbcdc4de81397a8511cb77d229
sharath <sharathvasa@gmail.com>
2018-04-11 21:05:30 -0400

    Docs: Repo for technical interview problems

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,38 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+//function reverse(str) {
+//    return str.split('').reverse().join('');
+//}
+
+/* function reverse(str) {
+  let reversed = '';
+  for(let character of str){
+      reversed = character + reversed;
+  }
+  return reversed;
+} */
+
+/* function reverse(str) {
+  str.split('').reduce((reversed, character) => {
+     return character + reversed;
+  }, '');     
+} */
+
+/* function reverse(str){
+   return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0);
+} */
+
+/* function reverse(str){
+  return str.split("").reduce((rev, char)=> char + rev, ''); 
+} */
+
+function reverse(str){
+  return [...str].reverse().join('');
+}
+module.exports = reverse;
