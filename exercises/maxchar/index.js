594d3c5aebaf780800800533aa17ac3cc1cb0ae8
sharath <sharathvasa@gmail.com>
2018-04-15 18:28:34 -0400

    feat: Added solution for maxchars of a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    let max = 0, maxChar = '';
+    for(let char of str){
+    chars[char] = chars[char]++ || 1; 
+    }
+    for(let char in chars){
+    if(chars[char] > max){
+    max = chars[char];
+    maxChar = char;
+    }   
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
