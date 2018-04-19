134454672326e2f9237d6cf525a4439c45ac42c5
sharath <sharathvasa@gmail.com>
2018-04-18 21:44:05 -0400

    feat: Added multiple solutions for the anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/* function anagrams(stringA, stringB) {
+const acharMap = buildCharMap(stringA);
+const bcharMap = buildCharMap(stringB);
+if (Object.keys(acharMap).length !== Object.keys(bcharMap).length) {
+return false;
+}
+for (let char in acharMap) {
+if (acharMap[char] !== bcharMap[char]) {
+return false;
+}
+}
+return true;
+}
 
-module.exports = anagrams;
+function buildCharMap(str) {
+const charMap = {};
+for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+charMap[char] = charMap[char] + 1 || 1;
+}
+return charMap;
+} */
+
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g).toLowerCase().split('').sort().join('');
+	}
+	module.exports = anagrams;
