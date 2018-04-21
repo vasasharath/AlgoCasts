5f7939f5a05ec403ef1280fd6c83bf24cc3763fe
sharath <sharathvasa@gmail.com>
2018-04-21 17:00:54 -0400

    feat: Added multiple solutions for the pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,36 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/* function pyramid(n) {
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	for (let i = 0; i < n; i++) {
+		let level = '';
+		for (let j = 0; j < 2 * n - 1; j++) {
+			if (midpoint - i <= j && midpoint + i >= j) {
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+        console.log(level);
+	}
+} */
+
+function pyramid(n, row = 0, level = '') {
+	if (row === n) {
+		return;
+	}
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		level += '#';
+	} else {
+		level += ' ';
+	}
+	pyramid(n, row, level);
+}
 
 module.exports = pyramid;
