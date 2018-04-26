3cb5787353482eae48f3a94111d496ee1ae77088
sharath <sharathvasa@gmail.com>
2018-04-25 21:26:53 -0400

    feat:added memoized solution to the fibonacci problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -27,10 +27,25 @@
 		return add;
 } */
 
-function fib(n) {
- if (n < 2)
-     return n;
- else
-     return fib (n - 1) + fib (n - 2);
+function memoize(fn) {
+    const cache = {};
+	return function (...args) {
+        if(cache[args]){
+        return cache[args];
+        }
+        const result = fn.apply(this,args);
+        cache[args] = result;
+        return result;
+    };
 }
+
+function slowFib(n) {
+	if (n < 2)
+		return n;
+	else
+		return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
 module.exports = fib;
