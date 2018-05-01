7883d1b6ba9fe4728cf20faad62cceec353ff5b0
sharath <sharathvasa@gmail.com>
2018-04-30 21:18:00 -0400

    feat: Added solutions to specific LinkedList functions

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,79 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor() {
+		this.head = null;
+	}
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+	}
+	size() {
+		let counter = 0;
+		let node = this.head;
+		while (node) {
+			counter++;
+			node = node.next;
+		}
+		return counter;
+	}
+	getFirst() {
+		return this.head;
+	}
+	getLast() {
+        if(!this.head){
+        return null;
+        }
+        let node = this.head;
+        while (node) {
+            if(!node.next){
+            return node;
+            }
+			node = node.next;
+		}
+    }
+    clear(){
+     this.head = null;
+    }
+    removeFirst(){
+        if(!this.head){
+        return;
+        }
+    this.head = this.head.next;
+    
+    }
+    removeLast(){
+    if(!this.head){
+        return;
+    }
+    if(!this.head.next){
+    this.head = null;
+    return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while(node.next){
+    previous = node;
+    node = node.next;
+    }
+    previous.next = null;
+    }
+    insertLast(data){
+    if(!this.head){
+        this.head =  new Node(data);
+    }
+    const last = this.getLast();
+    if(last){
+    last.next = new Node(data);
+    }
+    }
+}
 
-module.exports = { Node, LinkedList };
+module.exports = { Node, LinkedList};
