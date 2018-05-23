4f233a38d7bfc355d5633d256d5a8c201957c063
sharath <sharathvasa@gmail.com>
2018-05-23 16:16:54 -0400

    feat: Implemented TwoWaySort to accept negative numbers

--- exercises/twoWaySort/index.js
+++ exercises/twoWaySort/index.js
@@ -3,21 +3,45 @@ the first part of the array contains odd numbers sorted in descending order,
 rest portion contains even numbers sorted in ascending order. */
 
 function twoWaySort(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 === 1 || arr[i] % 2 === -1) {
-			arr[i] *= -1;
+	let l = 0,
+	r = arr.length - 1,
+	k = 0;
+	while (l < r) {
+		if (arr[l] % 2 != 0) {
+			l++;
+			k++;
+		}
+		// Find first even number from right side.
+		while (arr[r] % 2 == 0 && l < r)
+			r--;
+
+		// Swap odd number present on left and even
+		// number right.
+		if (l < r) {
+			arr[l] = arr[l] + arr[r];
+			arr[r] = arr[l] - arr[r];
+			arr[l] = arr[l] - arr[r];
+		}
+	}
+	for (let i = 0; i < k; i++) {
+		for (let j = i + 1; j < k; j++) {
+			if (arr[j] < arr[i]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
 		}
 	}
 
-	arr = arr.sort(function (a, b) {
-			return a - b;
-		});
-
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 === -1 || arr[i] % 2 === 1) {
-			arr[i] *= -1;
+	for (let i = k + 1; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] > arr[i]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
 		}
 	}
-
 	return arr;
 }
+console.log(twoWaySort([-1, -2, -3, -5, -4, -7, -10]));
