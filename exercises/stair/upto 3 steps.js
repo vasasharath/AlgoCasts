53fb06b6f8579847e1497d8bab959347a1b978d5
sharath <sharathvasa@gmail.com>
2018-06-18 18:13:24 -0400

    docs: added comments to improve code readability

--- exercises/stair/upto 3 steps.js
+++ exercises/stair/upto 3 steps.js
@@ -8,6 +8,7 @@ function countWays(n)
     res[0] = 1;
     res[1] = 1;
     res[2] = 2;
+    //for each number add previous 3 numbers
     for (let i = 3; i <= n; i++) 
         res[i] = res[i-1] + res[i-2] 
                           + res[i-3];
