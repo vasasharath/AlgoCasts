0885b75eff6e2fcbeb777ce458ee6a1bb74a9dfc
sharath <sharathvasa@gmail.com>
2018-07-04 21:27:35 -0400

    feat: added solution to the deletion distance problem

---
+++ exercises/deletion distance/index.js
@@ -0,0 +1,39 @@
+/*
+The deletion distance of two strings is the minimum number of characters 
+you need to delete in the two strings in order to get the same string.
+For instance, the deletion distance between "heat" and "hit" is 3:
+
+By deleting 'e' and 'a' in "heat", and 'i' in "hit", we get the string "ht" in both cases.
+We cannot get the same string from both strings by deleting 2 letters or fewer.
+*/
+const deletionDistance = function(a, b){
+  if(a.length == 0) return b.length; 
+  if(b.length == 0) return a.length; 
+
+  var matrix = [];
+
+  // increment along the first column of each row
+  var i;
+  for(i = 0; i <= b.length; i++){
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  var j;
+  for(j = 0; j <= a.length; j++){
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for(i = 1; i <= b.length; i++){
+    for(j = 1; j <= a.length; j++){
+      if(b.charAt(i-1) == a.charAt(j-1)){
+        matrix[i][j] = matrix[i-1][j-1];
+      } else {
+        matrix[i][j] = Math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1); // deletion
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+};
