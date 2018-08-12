cd641e19ab35b5a655ea067b8eeca3de060826ba
sharath <sharathvasa@gmail.com>
2018-08-11 22:57:44 -0400

    feat: added solution to the single number problem

---
+++ exercises/single number/index.js
@@ -0,0 +1,24 @@
+/*
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+*/
+
+var singleNumber = function(nums) {
+    for(let i of nums){
+        if(nums.indexOf(i) === nums.lastIndexOf(i)){
+            return i;
+        }
+    }
+};
