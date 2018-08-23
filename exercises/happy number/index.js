e4827955b8cadae47c20b65052b5956648357439
sharath <sharathvasa@gmail.com>
2018-08-23 12:23:18 -0400

    feat:added solution to the happy number problem

---
+++ exercises/happy number/index.js
@@ -0,0 +1,30 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: 
+Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+var isHappy = function(n) {
+    if(n === 1 || n === 7) return true;
+    if(n < 10) return false;
+    n = n.toString();
+    var nextNum = 0, tmp;
+    for(var i = 0; i < n.length; i++){
+        tmp = Math.floor(n[i]);
+        nextNum += tmp * tmp;
+    }
+    return isHappy(nextNum);
+};
