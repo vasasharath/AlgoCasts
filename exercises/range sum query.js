3f1af212d1cef78bbd93cfa54dfc676e4a9c267b
sharath <sharathvasa@gmail.com>
2018-09-17 11:14:08 -0400

    feat: added solution to the problem range sum query

---
+++ exercises/range sum query.js
@@ -0,0 +1,29 @@
+/*
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+*/
+var NumArray = function(nums) {
+    this.arr = nums;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    let sum = 0
+    while(i <= j){
+        sum += this.arr[i];
+        i++;
+    }
+    return sum;
