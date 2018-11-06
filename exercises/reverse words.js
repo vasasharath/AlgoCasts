251f21303d7a07db68981d1223ce0779fce3971f
vasasharath <sharathvasa@gmail.com>
2018-11-06 15:14:07 -0500

    feat: added solution to the problem reverse words

---
+++ exercises/reverse words.js
@@ -0,0 +1,20 @@
+/*
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+*/
+var reverseWords = function(s) {
+     let res = '';
+    let word = '';
+    for (let c of s) {
+        if (c === ' ') {
+            res += word + c;
+            word = '';
+        } else {
+            word = c + word;
+        }
+    }
+    return res + word;
+};
