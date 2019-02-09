88126f67da85e8b74bcc9d5b889793e9508885e2
vasasharath <sharathvasa@gmail.com>
2019-02-09 17:13:17 -0500

    Feat: Added solution to the problem toGoatLatin

---
+++ exercises/toGoatLatin.js
@@ -0,0 +1,54 @@
+/*
+A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+The rules of Goat Latin are as follows:
+
+If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+For example, the word 'apple' becomes 'applema'.
+ 
+If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+ 
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+Return the final sentence representing the conversion from S to Goat Latin. 
+
+ 
+
+Example 1:
+
+Input: "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+Example 2:
+
+Input: "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ 
+
+Notes:
+
+S contains only uppercase, lowercase and spaces. Exactly one space between each word.
+1 <= S.length <= 150.
+*/
+var toGoatLatin = function(S) {
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    var output = '';
+    var suffix = 'maa';
+    var moved = '';
+    for (var i=0; i<S.length; i++) {
+        if (S[i] === ' ') {
+            output += ' ';
+            suffix += 'a';
+            moved = '';
+        } else if (i+1 === S.length || S[i+1] === ' ') {
+            output += S[i] + moved + suffix;
+        } else if ((i === 0 || S[i-1] === ' ') && vowels.indexOf(S[i]) === -1) {
+            moved = S[i];
+        } else {
+            output += S[i];
+        }
+    }
+    return output;
+};
