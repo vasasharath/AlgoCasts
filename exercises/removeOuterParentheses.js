db0aed7115ae6437c06afb507fc0d46908019e7c
vasasharath <sharathvasa@gmail.com>
2019-04-11 11:35:50 -0400

    Feat: Added solution to the problem removeOuterParentheses

---
+++ exercises/removeOuterParentheses.js
@@ -0,0 +1,59 @@
+/*
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+*/
+var removeOuterParentheses = function(S) {
+    let res = []; //string buffer
+    let tmpCnt =0;
+    let tmpStr = '';
+    for(const c of S){
+        if(c ==='('){
+            ++tmpCnt
+        } else if(c ===')'){
+            --tmpCnt;
+        }
+        tmpStr +=c;
+        if(tmpCnt ===0){
+            res.push(tmpStr.substring(1, tmpStr.length-1));
+            tmpStr='';
+        }
+        
+    }
+    return res.join('');
+};
