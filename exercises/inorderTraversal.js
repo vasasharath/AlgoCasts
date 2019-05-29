ecb648b4684c5645c61823f0fff24d0017ce9469
vasasharath <sharathvasa@gmail.com>
2019-05-29 14:12:44 -0400

    Feat: Added solution to the problem inorderTraversal

---
+++ exercises/inorderTraversal.js
@@ -0,0 +1,32 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+var inorderTraversal = function(root) {
+    const stack = [];
+  const res = [];
+
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      res.push(root.val);
+      root = root.right;
+    }
+  }
+
+  return res;
+};
