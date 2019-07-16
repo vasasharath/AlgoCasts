52534203e91777d00771f950033ee753a78c1801
vasasharath <sharathvasa@gmail.com>
2019-07-15 18:15:27 -0700

    Feat: Added solution to the problem combinationSum

---
+++ exercises/combinationSum.js
@@ -0,0 +1,54 @@
+/*
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+*/
+var combinationSum = function(candidates, target) {
+      if (!candidates || !candidates.length) {
+        return []; 
+    }   
+
+    let result = []; 
+
+    function dfs(candidates, tmpArr, remain, start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            result.push(tmpArr.slice());
+            return;
+        } else {
+            for (let i = start; i < candidates.length; i++) {
+                tmpArr.push(candidates[i]);
+                dfs(candidates, tmpArr, remain - candidates[i], i); 
+                tmpArr.pop();
+            }   
+        }   
+    }   
+
+    dfs(candidates, [], target, 0); 
+
+    return result;
+};
