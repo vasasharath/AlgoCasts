7d37f5dd2942df731777faa2879b7060b3d025f7
vasasharath <sharathvasa@gmail.com>
2019-09-22 13:14:41 -0700

    Feat: Added solution to the problem fractionToDecimal

---
+++ exercises/fractionToDecimal.js
@@ -0,0 +1,49 @@
+/*
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+Example 1:
+
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+Example 2:
+
+Input: numerator = 2, denominator = 1
+Output: "2"
+Example 3:
+
+Input: numerator = 2, denominator = 3
+Output: "0.(6)"
+*/
+var fractionToDecimal = function(numerator, denominator) {
+  if (numerator === 0) return '0';
+  let res = '';
+
+  if (Math.sign(numerator) !== Math.sign(denominator)) res += '-';
+
+  let n = Math.abs(numerator);
+  const d = Math.abs(denominator);
+
+  res += Math.floor(n / d);
+  n %= d;
+
+  if (n === 0) return res;
+
+  res += '.';
+
+  let map = {};
+
+  while (n !== 0) {
+    map[n] = res.length;
+
+    n *= 10;
+    res += Math.floor(n / d);
+    n %= d;
+
+    const i = map[n]; // repeat starting index
+    if (i !== undefined) return `${res.substr(0, i)}(${res.substr(i)})`;
+  }
+
+  return res;
+}
