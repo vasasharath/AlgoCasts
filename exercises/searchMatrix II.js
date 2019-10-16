c47bc790b77d27fe9586e7f065ff7f92365adc35
vasasharath <sharathvasa@gmail.com>
2019-10-15 22:33:14 -0700

    Feat: Added solution to the problem var find = false;

---
+++ exercises/searchMatrix II.js
@@ -0,0 +1,32 @@
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+*/
+var searchMatrix = function(matrix, target) {
+   if (matrix.length < 1) {return false;}
+  if (matrix[0] && matrix[0][0] <= target &&  matrix[0][matrix[0].length-1] >= target) {
+    for (let i = 0; i < matrix[0].length; i++) {
+      if (matrix[0][i] === target) {return true;}
+    }
+  }
+  // remove first array in matrix 
+  matrix.shift();
+  // recurse 
+  return searchMatrix(matrix, target);
+};
