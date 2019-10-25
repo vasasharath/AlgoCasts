4ee86d16da9f76083f00de178eea86a11d8ba9a4
vasasharath <sharathvasa@gmail.com>
2019-10-24 17:57:38 -0700

    Feat: added solution to the problem NumMatrix

---
+++ exercises/NumMatrix.js
@@ -0,0 +1,45 @@
+/*
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+*/
+var NumMatrix = function(matrix) {
+    this.matrix = matrix;
+    var width = matrix.length ? matrix[0].length : 0;
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 1; j < width; j++) {
+            matrix[i][j] += matrix[i][j - 1];
+        }
+    }
+};
+
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
+    var sum = 0;
+    
+    for (var i = row1; i <= row2; i++) {
+        sum += this.matrix[i][col2];
+        if (col1 !== 0) {
+            sum -= this.matrix[i][col1 - 1];
+        }
+    }
+    
+    return sum;
+};
