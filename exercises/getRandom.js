0d29c6cad1817c6a3bfcdb68936ddcba22a92e38
vasasharath <sharathvasa@gmail.com>
2019-11-24 12:46:15 -0700

    Feat: added solution to the problrm getRandom

---
+++ exercises/getRandom.js
@@ -0,0 +1,30 @@
+/*
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+*/
+var Solution = function(head) {
+    this.list = [];
+    var cur = head;
+    while(cur!=null){
+        this.list.push(cur.val);
+        cur = cur.next;
+    }
+};
+
+Solution.prototype.getRandom = function() {
+   let rand = Math.floor(Math.random()*(this.list.length));
+   return this.list[rand];
+};
