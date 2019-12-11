4419520663dff051c602a1df6d70293651b0da1c
vasasharath <sharathvasa@gmail.com>
2019-12-11 09:07:21 -0700

    Feat: added solution to the problrm originalDigits

---
+++ exercises/originalDigits.js
@@ -0,0 +1,48 @@
+/*
+Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+
+Note:
+Input contains only lowercase English letters.
+Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as 
+"abc" or "zerone" are not permitted.
+Input length is less than 50,000.
+Example 1:
+Input: "owoztneoer"
+
+Output: "012"
+Example 2:
+Input: "fviefuro"
+
+Output: "45"
+*/
+var originalDigits = function(s) {
+      var count = Array(10);
+    count.fill(0);
+        for(var i = 0; i < s.length; i++) {
+            var c = s[i];
+            if (c == 'z') count[0]++;
+            if (c == 'w') count[2]++;
+            if (c == 'x') count[6]++;
+            if (c == 's') count[7]++; //7-6
+            if (c == 'g') count[8]++;
+            if (c == 'u') count[4]++; 
+            if (c == 'f') count[5]++; //5-4
+            if (c == 'h') count[3]++; //3-8
+            if (c == 'i') count[9]++; //9-8-5-6
+            if (c == 'o') count[1]++; //1-0-2-4
+        }
+        
+        count[7] -= count[6];
+        count[5] -= count[4];
+        count[3] -= count[8];
+        count[9] = count[9] - count[8] - count[5] - count[6];
+        count[1] = count[1] - count[0] - count[2] - count[4];
+        
+        var sb = "";
+        for(var i = 0; i <= 9; i++) {
+            for(var j = 0; j < count[i]; j++) {
+                sb+= i;
+            }
+        }
+        return sb;
+};
