3c994ebcd34f441ba0d0e934857964f2f5dd7d55
vasasharath <sharathvasa@gmail.com>
2020-06-15 07:01:14 -0700

    Feat: Added solution to the problem isReflected

---
+++ exercises/isReflected.js
@@ -0,0 +1,48 @@
+/*
+Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points symmetrically, in other words, answer whether or not if there exists a line that after reflecting all points over the given line the set of the original points is the same that the reflected ones.
+
+Note that there can be repeated points.
+
+Follow up:
+Could you do better than O(n2) ?
+
+ 
+
+Example 1:
+
+
+Input: points = [[1,1],[-1,1]]
+Output: true
+Explanation: We can choose the line x = 0.
+Example 2:
+
+
+Input: points = [[1,1],[-1,-1]]
+Output: false
+Explanation: We can't choose a line.
+ 
+
+Constraints:
+
+n == points.length
+1 <= n <= 10^4
+-10^8 <= points[i][j] <= 10^8
+*/
+var isReflected = function(points) {
+    let max= -Infinity, min= +Infinity, sum=0, length = points.length;
+    let set = new Set();
+    for(let i=0; i<length; i++){
+        max = Math.max(max, points[i][0]);
+        min = Math.min(min, points[i][0]);
+        let curStr= points[i][0] + "a" + points[i][1];
+        set.add(curStr);
+    }
+    sum= max+min;
+    for(let j=0; j<length; j++){
+        let cur= (sum-points[j][0]) + "a" +  points[j][1];
+        if(!set.has(cur)){
+            return false;
+        }
+    }
+    return true;
+};
