764c8fb7d94d9f9c0dfe293a234b4ed49769e85c
vasasharath <sharathvasa@gmail.com>
2020-08-04 12:07:57 -0700

    Feat: Added solution to the problem parseTernary

---
+++ exercises/parseTernary.js
@@ -0,0 +1,58 @@
+/*
+Given a string representing arbitrarily nested ternary expressions, calculate the result of the expression. You can always assume that the given expression is valid and only consists of digits 0-9, ?, :, T and F (T and F represent True and False respectively).
+
+Note:
+
+The length of the given string is ≤ 10000.
+Each number will contain only one digit.
+The conditional expressions group right-to-left (as usual in most languages).
+The condition will always be either T or F. That is, the condition will never be a digit.
+The result of the expression will always evaluate to either a digit 0-9, T or F.
+Example 1:
+
+Input: "T?2:3"
+
+Output: "2"
+
+Explanation: If true, then result is 2; otherwise result is 3.
+Example 2:
+
+Input: "F?1:T?4:5"
+
+Output: "4"
+
+Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+             "(F ? 1 : (T ? 4 : 5))"                   "(F ? 1 : (T ? 4 : 5))"
+          -> "(F ? 1 : 4)"                 or       -> "(T ? 4 : 5)"
+          -> "4"                                    -> "4"
+Example 3:
+
+Input: "T?T?F:5:3"
+
+Output: "F"
+
+Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+             "(T ? (T ? F : 5) : 3)"                   "(T ? (T ? F : 5) : 3)"
+          -> "(T ? F : 3)"                 or       -> "(T ? F : 5)"
+          -> "F"                                    -> "F"
+*/
+function parseTernary(expression) {
+  const N = expression.length;
+  const stack = [expression[N-1]];
+  for (let i = N-2; i >= 0; i -= 2) {
+    if (expression[i] === ':') {
+      stack.push(expression[i-1]);
+    } else {
+      const l = stack.pop();
+      const r = stack.pop();
+      if (expression[i-1] === 'T') {
+        stack.push(l);
+      } else {
+        stack.push(r);
+      }
+    }
+  }
+  return stack[0];
+}
