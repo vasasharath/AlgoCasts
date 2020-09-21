a3e18e91e9327d71b3bfac3f772fba7a5165d615
vasasharath <sharathvasa@gmail.com>
2020-09-21 05:40:08 -0700

    Feat: Added solution to the problem MaxStack

---
+++ exercises/MaxStack.js
@@ -0,0 +1,44 @@
+/*
+Design a max stack that supports push, pop, top, peekMax and popMax.
+
+push(x) -- Push element x onto stack.
+pop() -- Remove the element on top of the stack and return it.
+top() -- Get the element on the top.
+peekMax() -- Retrieve the maximum element in the stack.
+popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+Example 1:
+MaxStack stack = new MaxStack();
+stack.push(5); 
+stack.push(1);
+stack.push(5);
+stack.top(); -> 5
+stack.popMax(); -> 5
+stack.top(); -> 1
+stack.peekMax(); -> 5
+stack.pop(); -> 1
+stack.top(); -> 5
+Note:
+-1e7 <= x <= 1e7
+Number of operations won't exceed 10000.
+The last four operations won't be called when stack is empty.
+*/
+var MaxStack = function() {
+    this.arr=[];
+};
+MaxStack.prototype.push = function(x) {
+    this.arr.push(x);
+};
+MaxStack.prototype.pop = function() {
+    return this.arr.pop();
+};
+MaxStack.prototype.top = function() {
+    return this.arr[this.arr.length-1];
+};
+MaxStack.prototype.peekMax = function() {
+    return Math.max(...this.arr);
+};
+MaxStack.prototype.popMax = function() {
+    var max=Math.max(...this.arr);
+    var index=this.arr.lastIndexOf(max);
+    return this.arr.splice(index,1);
+};
