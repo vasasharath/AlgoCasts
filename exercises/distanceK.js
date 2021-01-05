218b61e787098214c0284663d45986d0d57d8f39
vasasharath <sharathvasa@gmail.com>
2021-01-04 20:01:19 -0500

    Feat: Added solution to the problem distanceK

---
+++ exercises/distanceK.js
@@ -0,0 +1,59 @@
+/*
+We are given a binary tree (with root node root), a target node, and an integer value K.
+
+Return a list of the values of all nodes that have a distance K from the target node.  The answer can be returned in any order.
+
+ 
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+
+Output: [7,4,1]
+
+Explanation: 
+The nodes that are a distance 2 from the target node (with value 5)
+have values 7, 4, and 1.
+
+
+
+Note that the inputs "root" and "target" are actually TreeNodes.
+The descriptions of the inputs above are just serializations of these objects.
+ 
+
+Note:
+
+The given tree is non-empty.
+Each node in the tree has unique values 0 <= node.val <= 500.
+The target node is a node in the tree.
+0 <= K <= 1000.
+*/
+var distanceK = function(root, target, K) {
+  if(!root) return []
+  const node = findTarget(root, null, target)  
+  const res = []
+  findAllKApart(node, K, res)
+  return res
+};
+
+function findTarget(root, parent, target){
+  if(!root) return null
+  root.parent = parent
+  if(root === target){    
+   return root 
+  }    
+  return findTarget(root.left, root, target) || findTarget(root.right, root, target)    
+}
+
+function findAllKApart(root, k, res){
+  if(!root || root.visited) return res
+  if(k == 0){
+    res.push(root.val)
+    return res
+  }   
+  root.visited = true
+  findAllKApart(root.left, k-1, res)
+  findAllKApart(root.right, k-1, res)
+  findAllKApart(root.parent, k-1, res)
+  return res
+}
