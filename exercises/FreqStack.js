4f414f0c826feb055a45e16b1e6ea5302949b49c
vasasharath <sharathvasa@gmail.com>
2021-01-20 21:07:54 -0500

    Feat: Added solution to the problem FreqStack

---
+++ exercises/FreqStack.js
@@ -0,0 +1,68 @@
+/*
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six .push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].
+
+pop() -> returns 7, as 5 and 7 is the most frequent, but 7 is closest to the top.
+The stack becomes [5,7,5,4].
+
+pop() -> returns 5.
+The stack becomes [5,7,4].
+
+pop() -> returns 4.
+The stack becomes [5,7].
+ 
+
+Note:
+
+Calls to FreqStack.push(int x) will be such that 0 <= x <= 10^9.
+It is guaranteed that FreqStack.pop() won't be called if the stack has zero elements.
+The total number of FreqStack.push calls will not exceed 10000 in a single test case.
+The total number of FreqStack.pop calls will not exceed 10000 in a single test case.
+The total number of FreqStack.push and FreqStack.pop calls will not exceed 150000 across all test cases.
+*/
+class FreqStack{
+    constructor(){
+        this.freq = new Map();
+        this.reverseMap = new Map();
+        this.maxCounter = 0;
+    }
+    // O(1)
+    push(x){
+        this.freq.set(x, (this.freq.get(x) || 0) + 1);
+        let frequency = this.freq.get(x);
+        if (this.maxCounter < frequency){
+            this.maxCounter = frequency
+        }
+        if (!this.reverseMap.has(frequency)){
+            this.reverseMap.set(frequency, []);
+        }
+        this.reverseMap.get(frequency).push(x);
+    }
+    // O(1)
+    pop(){
+        let mostFreq = this.reverseMap.get(this.maxCounter).pop();
+        while (this.reverseMap.get(this.maxCounter) && !this.reverseMap.get(this.maxCounter).length){
+            this.maxCounter--;
+        }
+        this.freq.set(mostFreq, this.freq.get(mostFreq) - 1);
+        return mostFreq;
+    }
+}
